48260de219c31d5ac82c6b1ea464b07843aab4a5
Jean-Philippe Bidegain <jp@bidega.in>
2021-09-04 14:15:26 +0200

    feat: show recent pastes first

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,11 +1,13 @@
 use rbatis::crud::CRUD;
-use crate::{paste::Paste, state::State, template::IndexTemplate};
+use crate::{state::State, template::IndexTemplate};
 use tide::{Request, Response};
 
 pub async fn index(req: Request<State>) -> tide::Result<Response> {
     let state = req.state();
     let pool = state.pool.lock().await;
-    let pastes = match pool.fetch_list::<Paste>().await {
+
+    let wrapper = pool.new_wrapper().order_by(false, &["id"]);
+    let pastes = match pool.fetch_list_by_wrapper(&wrapper).await {
         Ok(pastes) => pastes,
         Err(_err) => vec![],
     };
