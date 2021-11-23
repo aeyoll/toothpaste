a9204394cf824454d1f95dc47d779b02cb7dcb2a
Jean-Philippe Bidegain <jp@bidega.in>
2021-11-23 07:45:11 +0100

    feat: add the cleanup logic

--- src/routes/cleanup.rs
+++ src/routes/cleanup.rs
@@ -1,6 +1,20 @@
+use crate::paste::Paste;
 use crate::state::State;
-use tide::Request;
+use chrono::NaiveDateTime;
+use rbatis::core::value::DateTimeNow;
+use rbatis::crud::CRUD;
+use tide::{Request, Response};
 
-pub async fn cleanup(_req: Request<State>) -> tide::Result {
-    Ok(format!("Cleanup finished").into())
+pub async fn cleanup(req: Request<State>) -> tide::Result<Response> {
+    let state = req.state();
+    let pool = state.pool.lock().await;
+
+    let now = NaiveDateTime::now();
+    let wrapper = pool
+        .new_wrapper()
+        .gt("expire_after", 0)
+        .lt("expire_time", now);
+    let _ = pool.remove_by_wrapper::<Paste>(&wrapper).await;
+
+    Ok("Cleanup finished".to_string().into())
 }
