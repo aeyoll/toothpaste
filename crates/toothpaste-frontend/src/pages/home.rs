9bc6f013a436f833a84b45812a796f1465606cff
Jean-Philippe Bidegain <jp@bidega.in>
2024-10-17 16:32:55 +0200

    feat: start integration

--- crates/toothpaste-frontend/src/pages/home.rs
+++ crates/toothpaste-frontend/src/pages/home.rs
@@ -12,9 +12,9 @@ impl Component for Home {
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div>
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 { "Home" }
             </div>
         }
     }
-    }
+}
