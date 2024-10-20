5307c21296ae90fd96ee9ce29450bd42f86ef951
Jean-Philippe Bidegain <jp@bidega.in>
2024-10-20 11:18:07 +0200

    feat: allow to filter log messages using compile time env variable

--- crates/toothpaste-frontend/src/main.rs
+++ crates/toothpaste-frontend/src/main.rs
@@ -4,6 +4,7 @@ mod pages;
 
 use tracing_subscriber::fmt::format::Pretty;
 use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
 use tracing_web::{performance_layer, MakeWebConsoleWriter};
 
 use app::App;
@@ -17,6 +18,7 @@ fn main() {
     let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
 
     tracing_subscriber::registry()
+        .with(EnvFilter::from_env("TOOTHPASTE_LOG_LEVEL"))
         .with(fmt_layer)
         .with(perf_layer)
         .init();
