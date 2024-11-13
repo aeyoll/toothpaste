0342b682b87f6b53d30ca432f7e3f92e12e0d870
Jean-Philippe Bidegain <jp@bidega.in>
2024-11-13 12:18:43 +0100

    fix: update static images urls

--- crates/toothpaste-frontend/src/app.rs
+++ crates/toothpaste-frontend/src/app.rs
@@ -57,27 +57,27 @@ impl App {
                     <div class="relative flex h-16 items-center justify-between">
                         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                             <div class="flex flex-shrink-0 items-center">
-                                <img src="static/logo.svg" alt="Toothpaste logo" width="24" height="24" />
+                                <img src="/static/logo.svg" alt="Toothpaste logo" width="24" height="24" />
                             </div>
                             <div class="hidden sm:ml-6 sm:block">
                                 <div class="flex space-x-4">
                                     <RouteLink to={Route::NewPaste}>
                                         <div class="flex items-center text-white gap-2">
-                                            <img src="static/plus.svg" alt="New paste" width="18" height="18" />
+                                            <img src="/static/plus.svg" alt="New paste" width="18" height="18" />
                                             { "New paste" }
                                         </div>
                                     </RouteLink>
 
                                     <RouteLink to={Route::About}>
                                         <div class="flex items-center text-white gap-2">
-                                            <img src="static/info.svg" alt="About" width="18" height="18" />
+                                            <img src="/static/info.svg" alt="About" width="18" height="18" />
                                             { "About" }
                                         </div>
                                     </RouteLink>
 
                                     <a href="https://github.com/aeyoll/toothpaste" target="_blank" rel="noopener" class="rounded-md px-3 py-2 text-sm font-medium text-white">
                                         <div class="flex items-center text-white gap-2">
-                                            <img src="static/github.svg" alt="Github" width="18" height="18" />
+                                            <img src="/static/github.svg" alt="Github" width="18" height="18" />
                                             { "Github" }
                                         </div>
                                     </a>
