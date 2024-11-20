1ac12e4e92bc5b68af154ff4d96781e296cd1bec
Jean-Philippe Bidegain <jp@bidega.in>
2024-11-20 14:50:00 +0100

    feat: allow to log in the console by enabling a feature flag

--- crates/toothpaste-frontend/src/main.rs
+++ crates/toothpaste-frontend/src/main.rs
@@ -5,5 +5,11 @@ mod pages;
 use app::App;
 
 fn main() {
+    // If feature "log" is enabled, logs will be printed to the browser console
+    #[cfg(feature = "log")] {
+        wasm_logger::init(wasm_logger::Config::default());
+        log::debug!("Logging enabled");
+    }
+
     yew::Renderer::<App>::new().render();
 }
